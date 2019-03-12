5a7c23e8d6088fc119ae89cb2d42d03914948c90
Tim Bergquist <trberg@uw.edu>
2019-03-12 11:08:31 -0700

    added main file

---
+++ DQe-c.py
@@ -0,0 +1,11 @@
+from orphan import Orphan
+
+
+def test():
+    orph = Orphan("OMOPV5_0")
+
+    print (orph.primary())
+
+
+if __name__ == "__main__":
+    test()
--- orphan.py
+++ orphan.py
@@ -10,3 +10,36 @@ expected input: orphan_keys.csv
 
 expected output: orphan.csv
 """
+import pandas
+import json
+
+class Orphan:
+    def __init__(self, CDM: str):
+        with open('configs.json') as json_file:
+            self.config = json.load(json_file)
+        
+        self.DQTBL: object = {
+                                "PCORNET3": pandas.read_csv("./CDMs/DQTBL_pcornet_v3.csv"),
+                                "PCORNET31": pandas.read_csv("./CDMs/DQTBL_pcornet_v31.csv"),
+                                "OMOPV5_0": pandas.read_csv("./CDMs/DQTBL_omop_v5_0.csv"),
+                                "OMOPV5_2": pandas.read_csv("./CDMs/DQTBL_omop_v5_2.csv"),
+                                "OMOPV5_3": pandas.read_csv("./CDMs/DQTBL_omop_v5_3.csv"),
+                             }[CDM]
+
+
+    def primary(self):
+
+        ## gather primary keys from DQTBL
+        primaryPairs: object = self.DQTBL[(self.DQTBL["primary"]) & (self.DQTBL["cat"].isin(["clinical", "health_system"]))][["ColNam", "TabNam"]]
+        #print (primaryPairs)
+
+        ## gather all non-primary table-column pairs
+        referencePairs: object = self.DQTBL[(self.DQTBL["primary"] == False) & (self.DQTBL["cat"].isin(["clinical", "health_system"]))][["ColNam", "TabNam"]]
+        #print (referencePairs)
+
+        ## combine the two tables
+        mergedPairs = primaryPairs.merge(referencePairs, on="ColNam", how="right", suffixes=("_primary", "_reference"))
+
+        mergedPairs.dropna(subset = ["TabNam_primary"], inplace=True)
+        
+        return mergedPairs
