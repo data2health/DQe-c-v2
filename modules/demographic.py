11de50fd8955bc27fef1c19847d7f054d8749dc2
Mehadi Hassan <mehadi@Mehadis-MacBook-Pro.local>
2019-05-29 10:57:44 -0700

    adding demographic module

---
+++ modules/demographic.py
@@ -0,0 +1,8 @@
+# Demographic module (OMOP):
+# Race, gender, ethnicity, percentage of pop., organization site
+
+# Use from indicators
+
+class Demographic:
+    def __init__(self, query: object):
+        self.query = query
